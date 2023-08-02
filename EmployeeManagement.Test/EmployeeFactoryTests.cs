a66ea7ae441a9e6bca8849a0173c18f848df3f54
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-08-02 13:27:44 +0200

    Add unit tests for employee.Salary range: must be between 2500 and 3500

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -14,5 +14,36 @@ namespace EmployeeManagement.Test
 
             Assert.Equal(2500, employee.Salary);
         }
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500, "Salary must be between 2500 and 3500.");
+        }
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            Assert.True(employee.Salary >= 2500);
+            Assert.True(employee.Salary <= 3500);
+        }
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithInRange()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+
+            // Assert
+            Assert.InRange(employee.Salary, 2500, 3500);
+        }
     }
 }
