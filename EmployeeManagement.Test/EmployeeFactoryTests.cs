b37838738e4405a7b7dcf01044c7adb2cd843b38
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-08-02 15:03:06 +0200

    Add unit test with number precision: employee.Salary must be 2500, create EmployeeTestDataRepository, create EmployeeServiceTests, add unit tests: check internalEmployee.AttendedCourses should contain 2 courses

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -45,5 +45,18 @@ namespace EmployeeManagement.Test
             // Assert
             Assert.InRange(employee.Salary, 2500, 3500);
         }
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            employee.Salary = 2500.123m;  // 'm' is used to initialize the number as decimal instead of double (standard)
+
+            // Assert
+            Assert.Equal(2500, employee.Salary, 0);
+        }
     }
 }
