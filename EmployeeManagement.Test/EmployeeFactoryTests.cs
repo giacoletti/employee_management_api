53c251a856b90173889dd8e862dc434e18e9fcd3
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-08-03 10:15:26 +0200

    Refactor EmployeeFactoryTests: add constructor and dispose

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -3,32 +3,37 @@ using EmployeeManagement.DataAccess.Entities;
 
 namespace EmployeeManagement.Test
 {
-    public class EmployeeFactoryTests
+    public class EmployeeFactoryTests : IDisposable
     {
+        private EmployeeFactory _employeeFactory;
+        public EmployeeFactoryTests() 
+        {
+            _employeeFactory = new EmployeeFactory();
+        }
+
+        public void Dispose() 
+        {
+            // clean up the setup code, if required.
+        }
+
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
         {
-            var employeeFactory = new EmployeeFactory();
-
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
 
             Assert.Equal(2500, employee.Salary);
         }
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
         {
-            var employeeFactory = new EmployeeFactory();
-
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
 
             Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500, "Salary must be between 2500 and 3500.");
         }
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_Alternative()
         {
-            var employeeFactory = new EmployeeFactory();
-
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
 
             Assert.True(employee.Salary >= 2500);
             Assert.True(employee.Salary <= 3500);
@@ -36,11 +41,8 @@ namespace EmployeeManagement.Test
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithInRange()
         {
-            // Arrange
-            var employeeFactory = new EmployeeFactory();
-
             // Act
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
 
             // Assert
             Assert.InRange(employee.Salary, 2500, 3500);
@@ -48,11 +50,8 @@ namespace EmployeeManagement.Test
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
         {
-            // Arrange
-            var employeeFactory = new EmployeeFactory();
-
             // Act
-            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Kevin", "Dockx");
+            var employee = (InternalEmployee)_employeeFactory.CreateEmployee("Kevin", "Dockx");
             employee.Salary = 2500.123m;  // 'm' is used to initialize the number as decimal instead of double (standard)
 
             // Assert
@@ -61,11 +60,8 @@ namespace EmployeeManagement.Test
         [Fact]
         public void CreateEmployee_IsËxternalIsTrue_ReturnTypeMustBeExternalEmployee()
         {
-            // Arrange
-            var employeeFactory = new EmployeeFactory();
-
             // Act
-            var employee = employeeFactory.CreateEmployee("Kevin", "Dockx", "Marvin", true);
+            var employee = _employeeFactory.CreateEmployee("Kevin", "Dockx", "Marvin", true);
 
             // Assert
             Assert.IsType<ExternalEmployee>(employee);
