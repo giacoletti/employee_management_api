885347c1a64e6c955d380c1dfcc208464af80445
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-08-03 14:23:49 +0200

    Add Theory unit test with member data: GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue

--- EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
+++ EmployeeManagement.Test/DataDrivenEmployeeServiceTests.cs
@@ -14,6 +14,29 @@ namespace EmployeeManagement.Test
             _employeeServiceFixture = employeeServiceFixture;
         }
 
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithProperty 
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                    new object[] {100, true},
+                    new object[] {200, false},
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> ExampleTestDataForGiveRaise_WithMethod(int testDataInstancesToProvide)
+        {
+            var testData = new List<object[]>
+                {
+                    new object[] {100, true}, 
+                    new object[] {200, false},
+                };
+
+            return testData.Take(testDataInstancesToProvide);
+        }
+
         [Theory]
         [InlineData("1fd115cf-f44c-4982-86bc-a8fe2e4ff83e")]
         [InlineData("37e03ca7-c730-4351-834c-b66f280cdb01")]
@@ -55,6 +78,25 @@ namespace EmployeeManagement.Test
 
             // Assert
             Assert.False(internalEmployee.MinimumRaiseGiven);
+        }
+
+        [Theory]
+        [MemberData(
+            nameof(DataDrivenEmployeeServiceTests.ExampleTestDataForGiveRaise_WithMethod),
+            1,
+            MemberType = typeof(DataDrivenEmployeeServiceTests))]
+        public async Task GiveRaise_RaiseGiven_EmployeeMinimumRaiseGivenMatchesValue(int raiseGiven, bool expectedValueForMinimumRaiseGiven)
+        {
+            // Arrange  
+            var internalEmployee = new InternalEmployee(
+                "Brooklyn", "Cannon", 5, 3000, false, 1);
+
+            // Act 
+            await _employeeServiceFixture.EmployeeService
+                .GiveRaiseAsync(internalEmployee, raiseGiven);
+
+            // Assert
+            Assert.Equal(expectedValueForMinimumRaiseGiven, internalEmployee.MinimumRaiseGiven);
         }
     }
 }
